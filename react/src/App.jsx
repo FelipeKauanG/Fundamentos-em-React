e145f1e1fd2ea876829a881a5a79c1485fff2cd9
Felipe Kauan <144256901+FelipeKauanG@users.noreply.github.com>
2023-11-29 15:06:22 -0300

    renderizando o cubo

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,17 +1,44 @@
 import './App.css'
-
+import * as THREE from "three"
 function App() {
 
-  import * as THREE from "three"
 
   const scene = new THREE.Scene();
+
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-  const renderer = new
+  const renderer = new THREE.WebGLRenderer();
 
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  document.body.appendChild( renderer.domElement )
+  
+
+  const geometry = new THREE.BoxGeometry(1,1,1)
+
+  const material = new THREE.MeshBasicMaterial ({color: 0x1cbbc7 })
+
+  const cube = new THREE.Mesh( geometry, material)
+  scene.add(cube)
+
+
+  camera.position.z = 5
+
+  
+  function animate(){
+    requestAnimationFrame( animate )
+    renderer.render( scene, camera)
+    cube.rotation.x = 0.02
+    cube.rotation.y = 0.02
+
+  }
+
+  animate()
   return (
     <>
         <div className="btn btn-danger">Minecraft site</div>
+
+
     </>
   )
 }
