530d0962711becac85e4ad8509e6905f1c94c65b
Felipe Kauan <144256901+FelipeKauanG@users.noreply.github.com>
2023-11-29 15:39:03 -0300

    att

---
+++ react/src/componentes/cubo.jsx
@@ -0,0 +1,38 @@
+import * as THREE from "three"
+
+//declarar a cena
+const scene = new THREE.Scene()
+
+//declarar câmera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+//Declarar o renderizador
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+//renderizar a cena no arquivo .html
+document.body.appendChild(renderer.domElement)
+
+//declarar o objeto
+const geometry = new THREE.BoxGeometry(1,1,1)
+
+//declar o material
+const material = new THREE.MeshBasicMaterial({color: 0x00ffee})
+
+// criar o cubo
+const cube = new THREE.Mesh( geometry, material)
+scene.add(cube)
+
+//Posição da câmera
+camera.position.z = 5
+
+//fazer uma função de animação e renderizar
+function animate(){
+  requestAnimationFrame( animate)
+  cube.rotation.z += 0.01
+  cube.rotation.y += 0.01
+
+  renderer.render( scene, camera)
+}
+
+animate()
